fc08ef075d903d5b93da54ba731fdcf1a98e14b9
Alexey Bezdetko <ggalliosha@gmail.com>
2024-01-15 18:14:48 +0300

    feat(LWC): created sticky footer theme

---
+++ force-app/main/default/lwc/stickyFooterTheme/stickyFooterTheme.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+/**
+* @slot header Header slot 
+* @slot footer Footer slot
+* @slot default Content slot
+*/
+export default class StickyFooterTheme extends LightningElement {}
