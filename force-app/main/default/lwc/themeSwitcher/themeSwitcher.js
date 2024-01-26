931c3093e6ab3407bf435341cefb736b9fc53ca6
Alexey Bezdetko <ggalliosha@gmail.com>
2024-01-26 22:14:06 +0300

    feat: redesigned night theme switcher

--- force-app/main/default/lwc/themeSwitcher/themeSwitcher.js
+++ force-app/main/default/lwc/themeSwitcher/themeSwitcher.js
@@ -1,3 +1,31 @@
 import { LightningElement } from 'lwc'
+import { THEME, EVENT_NAME, STORAGE } from 'c/utils'
 
-export default class ThemeSwitcher extends LightningElement {}
+export default class ThemeSwitcher extends LightningElement {
+    _theme = THEME.LIGHT
+
+    connectedCallback() {
+        const theme = window.localStorage.getItem(STORAGE.THEME) || THEME.LIGHT
+
+        if (theme === THEME.LIGHT) return
+
+        this._theme = theme
+    }
+
+    get isChecked() {
+        return this._theme === THEME.DARK
+    }
+
+    handleTogglerChange(event) {
+        event.preventDefault()
+
+        this._theme = this._theme === THEME.LIGHT ? THEME.DARK : THEME.LIGHT
+
+        this.dispatchEvent(
+            new CustomEvent(EVENT_NAME.THEME_TOGGLE, {
+                bubbles: true,
+                composed: true,
+            })
+        )
+    }
+}
